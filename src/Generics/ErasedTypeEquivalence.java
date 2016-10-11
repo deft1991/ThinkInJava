d8fcba14997a433d9c2299bda522c811379d420b
deft1991 <deft1991@gmail.com>
2016-10-11 22:11:15 +0300

    Стирание типов

---
+++ src/Generics/ErasedTypeEquivalence.java
@@ -0,0 +1,14 @@
+package Generics;
+
+import java.util.ArrayList;
+
+/**
+ * Created by deft on 11.10.2016.
+ */
+public class ErasedTypeEquivalence {
+    public static void main(String[] args) {
+        Class c1 = new ArrayList<String>().getClass();
+        Class c2 = new ArrayList<Integer>().getClass();
+        System.out.println(c1 == c2);
+    }
+}
